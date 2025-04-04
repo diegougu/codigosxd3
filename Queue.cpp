68c82b518b1d56f896fbaf1e84fd902a375acb44
diegougu <128000056+diegougu@users.noreply.github.com>
2025-04-03 20:15:27 -0500

    Update Queue.cpp

--- Queue.cpp
+++ Queue.cpp
@@ -6,68 +6,84 @@ class Queue {
 private:
 	T arr[10];
 	T* head = arr;
-	T* tail = arr;
-	int elems = 0;
+	T* tail = arr - 1;
 public:
 	void push(T x);
 	void pop(T& e);
+	void print();
 };
 
 template <class T>
 void Queue<T>::push(T x) {
-	if ((head == arr && tail == arr + 10) || ((tail != arr && head != arr) && tail == head)) {
-		cout << "full" << endl;
+	if ((head == arr && tail == arr + 9) || ((tail != arr && head != arr) && tail == head)) {
 		return;
 	}
-	else if (head != arr && tail == arr + 10) {
+	else if (tail == arr + 9 && head != arr) {
 		tail = arr;
+		*tail = x;
 	}
-	*tail = x;
-	tail++;
-	elems++;
+	else {
+		tail++;
+		*tail = x;
+	}
 }
 
 template <class T>
 void Queue<T>::pop(T& e) {
-	if (elems == 0) {
+	if (head == tail) {
 		return;
 	}
 	else if (head == arr + 9) {
-		e = *head;
 		head = arr;
-		elems--;
+		e = *head;
 	}
 	else {
 		e = *head;
 		head++;
-		elems--;
 	}
 }
 
 
-
+template <class T>
+void Queue<T>::print() {
+	T* p = head;
+	while (p != tail) {
+		if (p == arr + 10 && p != tail) {
+			p = arr;
+		}
+		cout << *p << " ";
+		if (!(p == arr + 10 && p != tail)) {
+			p++;
+		}
+	}
+	cout << *tail << endl;
+}
 
 
 int main() {
 	Queue<int> q;
 	for (int i = 0; i < 15; i++) {
-		cout << i << endl;
 		q.push(i);
 	}
+	q.print();
 
 	for (int i = 0; i < 5; i++) {
 		int e = 0;
 		q.pop(e);
 		cout << e << endl;
 	}
+	q.print();
 
 	for (int i = 30; i < 35; i++) {
 		q.push(i);
 	}
+	q.print();
 
 	for (int i = 0; i < 1; i++) {
 		int e = 0;
 		q.pop(e);
 		cout << e << endl;
 	}
+	q.print();
+
 }
